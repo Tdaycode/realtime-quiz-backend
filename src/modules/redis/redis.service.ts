dcb72b375841c031fde9e027168f942b3b795981
Omotayo Ganiyu <tayorgudestryhero@gmail.com>
2025-10-08 12:57:47 +0100

    chore: removing unsed files

--- src/modules/redis/redis.service.ts
+++ src/modules/redis/redis.service.ts
@@ -14,6 +14,7 @@ export class RedisService implements OnModuleDestroy {
     const redisConfig = {
       host: this.configService.get<string>('REDIS_HOST', 'localhost'),
       port: this.configService.get<number>('REDIS_PORT', 6379),
+      password: this.configService.get<string>('REDIS_PASSWORD'),
       retryStrategy: (times: number) => {
         const delay = Math.min(times * 50, 2000);
         return delay;
